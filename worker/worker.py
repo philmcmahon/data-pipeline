8455022953bc0e82065a30db3c4b4bcf7b7db6bc
philmcmahon <philip.mcmahon@theguardian.com>
2026-05-26 17:26:39 +0100

    add whisperx

--- worker/worker.py
+++ worker/worker.py
@@ -5,6 +5,29 @@ import os
 import boto3
 import pika
 
+import whisperx
+import gc
+from whisperx.diarize import DiarizationPipeline
+
+
+# whisper settings
+device = "cuda"
+batch_size = 8 # reduce if low on GPU mem
+compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
+
+model = whisperx.load_model("large-v2", device, compute_type=compute_type)
+
+def transcribe_audio(file_path):
+    # Transcribe file with whisperx
+    result = model.transcribe(file_path, batch_size=batch_size)
+
+    # write vtt output to file
+    vtt_path = file_path + ".vtt"
+    with open(vtt_path, "w") as vtt_file:
+        vtt_file.write(result["vtt"])
+    print(f"Transcription complete. VTT file saved to: {vtt_path}")
+
+
 def consume_queue(queue_name, url):
     s3 = boto3.client("s3")
     connection = pika.BlockingConnection(pika.URLParameters(url))
@@ -23,6 +46,7 @@ def consume_queue(queue_name, url):
 
         s3.download_file(bucket, key, local_path)
         print(f"Downloaded: {local_path}")
+        transcribe_audio(local_path)
         os.remove(local_path)
 
         channel.basic_ack(delivery_tag=method.delivery_tag)
